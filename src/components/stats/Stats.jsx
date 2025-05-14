836b514d91523fe9b9d8c30f4086fb8a12f7ec79
Daniel Alejandro Cano Cardona <dacardonac201@gmail.com>
2025-05-14 12:05:22 -0500

    style: refine styles for Card and Stats components, enhancing layout and readability

--- src/components/stats/Stats.jsx
+++ src/components/stats/Stats.jsx
@@ -6,9 +6,9 @@ const Stats = () => {
       <h1 className={s["stats__title"]}>Get <span className={s["stats__title--violet"]}>insights</span> that help your business grow.</h1>
       <p className={s["stats__paragraph"]}>Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency.</p>
       <div className={s["stats__numbers"]}>
-        <div className={s["stats__numbers--companies"]}>10k+ <span className={s["stats__numbers--light"]}>companies</span></div>
-        <div className={s["stats__numbers--templates"]}>314 <span className={s["stats__numbers--light"]}>templates</span></div>
-        <div className={s["stats__numbers--queries"]}>12m+ <span className={s["stats__numbers--light"]}>queries</span></div>
+        <div className={s["stats__numbers--stats"]}><span className={s["stats__numbers--strong"]}>10k+</span> <span className={s["stats__numbers--light"]}>companies</span></div>
+        <div className={s["stats__numbers--stats"]}><span className={s["stats__numbers--strong"]}>314</span> <span className={s["stats__numbers--light"]}>templates</span></div>
+        <div className={s["stats__numbers--stats"]}><span className={s["stats__numbers--strong"]}>12m+</span> <span className={s["stats__numbers--light"]}>queries</span></div>
       </div>
     </section>
   );
