711e0585d815b2d7761580726f0b7c8325e05a02
Daniel Alejandro Cano Cardona <dacardonac201@gmail.com>
2025-06-03 12:36:28 -0500

    fix: image paths

--- src/components/illustration/Illustration.jsx
+++ src/components/illustration/Illustration.jsx
@@ -2,14 +2,14 @@ import { useState, useEffect } from 'react';
 import i from './Illustration.module.scss';
 
 const Illustration = ({ description }) => {
-  const [imageSrc, setImageSrc] = useState('images/image-header-desktop.webp');
+  const [imageSrc, setImageSrc] = useState('/images/image-header-desktop.webp');
 
   useEffect(() => {
     const updateImage = () => {
       if (window.innerWidth >= 1440) {
-        setImageSrc('images/image-header-desktop.webp');
+        setImageSrc('/images/image-header-desktop.webp');
       } else {
-        setImageSrc('images/image-header-mobile.webp');
+        setImageSrc('/images/image-header-mobile.webp');
       }
     };
     updateImage();
